fdd180af87998c0a835e1590e78a7d35468b6738
xdaavid <118130625+xdaavid@users.noreply.github.com>
2023-08-02 23:55:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ fetch('emg_data.csv')
         }
     })
     .catch(error => console.error('Error al cargar el archivo:', error));
+// Crear el gráfico de líneas con Chart.js
+        const ctx = document.getElementById('emg-chart').getContext('2d');
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: Array.from({ length: emg_data.length }, (_, i) => i + 1),
+                datasets: [{
+                    label: 'EMG Data',
+                    data: emg_data,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 2,
+                    fill: false
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Index'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'EMG Data'
+                        }
+                    }
+                }
+            }
+        });
+    })
+    .catch(error => console.error('Error al cargar el archivo:', error));
